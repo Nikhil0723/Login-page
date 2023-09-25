6515161a79bfd555151967d40b70277df8ec50dc
Nikhil Vishwakarma <122264427+Nikhilv47@users.noreply.github.com>
2023-09-25 07:40:47 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
-import Auth from './components/auth/Auth';
-
+import React,{useState} from 'react';
 function App() {
+
+ const [counter , setCounter] = useState(0)
+
   return (
     <div className="App">
-      <Auth/>
+<div className="header"><h1>Counter App</h1></div>
+<div className="counter"><h1>{counter}</h1></div>
+<div className="btn-container">
+  <button onClick={()=>{setCounter(counter + 1)}}>Increment</button>
+  <button onClick={()=>{setCounter(0)}} disabled={counter === 0}>Reset</button>
+  <button onClick={()=>{setCounter(counter - 1)}} disabled={counter === 0}>Decrement</button>
+</div>
     </div>
   );
 }
