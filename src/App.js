5d81ea0ac5b207d5db9e9639d177dfb52795ed7d
Nikhil Vishwakarma <122264427+Nikhilv47@users.noreply.github.com>
2023-09-27 17:42:33 +0530

    revert
    This reverts commit 6515161a79bfd555151967d40b70277df8ec50dc.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import './App.css';
-import React,{useState} from 'react';
+import Auth from './components/auth/Auth';
+
 function App() {
-
- const [counter , setCounter] = useState(0)
-
   return (
     <div className="App">
-<div className="header"><h1>Counter App</h1></div>
-<div className="counter"><h1>{counter}</h1></div>
-<div className="btn-container">
-  <button onClick={()=>{setCounter(counter + 1)}}>Increment</button>
-  <button onClick={()=>{setCounter(0)}} disabled={counter === 0}>Reset</button>
-  <button onClick={()=>{setCounter(counter - 1)}} disabled={counter === 0}>Decrement</button>
-</div>
+      <Auth/>
     </div>
   );
 }
